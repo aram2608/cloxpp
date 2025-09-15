849764d83a40b43b3147af10cb430b97019f6567
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-09-14 22:12:07 -0400

    cmake preset for managing both projects

--- make.sh
+++ make.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# We set a variable for the path to vcpkg
+# # We set a variable for the path to vcpkg and export to env
 VCPKG_ROOT="${VCPKG_ROOT:-$HOME/dev/tools/vcpkg}"
+export VCPKG_ROOT
 
-# We can then run our cmake build
-# We pass in the toolchain file and set the correct build directory
-cmake -B build -S . \
-    -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" \
-    -DTREE_WALK=OFF \
-    -DVM_BUILD=ON
+# Command line arg
+OPTION=$1
 
-# We can now build the project
-cmake --build build
+# Newbie code but we test if the proper args are passed in and run the respective
+# build mode
+if [[ -z $OPTION ]]; then
+    echo "Build script usage: "
+    echo "      1 = Tree walk interpreter debug build"
+    echo "      2 = Tree walk interpreter release build"
+    echo "      3 = VM interpreter debug build"
+    echo "      4 = VM interpreter release build"
+elif [[ $OPTION == 1 ]]; then
+    cmake --preset tw-debug
+    cmake --build --preset tw-debug
+elif [[ $OPTION == 2 ]]; then
+    cmake --preset tw-release
+    cmake --build --preset tw-release
+elif [[ $OPTION == 3 ]]; then
+    cmake --preset vm-debug
+    cmake --build --preset vm-debug
+elif [[ $OPTION == 4 ]]; then
+    cmake --preset vm-release
+    cmake --build --preset vm-release
+else
+    echo "Improper use. Integer arguments only"
+fi
